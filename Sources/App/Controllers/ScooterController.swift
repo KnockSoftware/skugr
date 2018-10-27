4d24be0a2a275fbb2040e89bf66f1766383c5425
William Henderson <william.c.henderson@gmail.com>
2018-10-27 13:47:27 -0700

    wrap the response body so it is fully GBFS compliant

--- Sources/App/Controllers/ScooterController.swift
+++ Sources/App/Controllers/ScooterController.swift
@@ -3,8 +3,27 @@ import FluentSQLite
 
 /// Controls basic CRUD operations on `Scooter`s.
 final class ScooterController {
+    struct AvailableScootersResponseData: Codable {
+        var bikes: [Scooter]
+    }
+    struct AvailableScootersResponse: Codable {
+        var ttl: Int
+        var last_updated: Int
+        var data : AvailableScootersResponseData
+    }
+    
     /// Returns a list of all `Scooter`s.
-    func availableScooters(_ req: Request) throws -> Future<[Scooter]> {
-        return Scooter.query(on: req).filter(\Scooter.reserved == false).all()
+    func availableScooters(_ req: Request) throws -> Future<String> {
+        return Scooter.query(on: req).filter(\Scooter.reserved == false).all().map() { scooters in
+            let response = AvailableScootersResponse(ttl: 0, last_updated: Int(Date().timeIntervalSinceReferenceDate), data: AvailableScootersResponseData(bikes: scooters))
+            
+            let jsonEncoder = JSONEncoder()
+            do {
+                let jsonData = try jsonEncoder.encode(response)
+                return String(data: jsonData, encoding: .utf8) ?? "FAIL"
+            } catch {
+                return "FAIL"
+            }
+        }
     }
 }
